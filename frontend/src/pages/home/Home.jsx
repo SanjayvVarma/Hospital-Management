53cc5ca786c10413604da3b3f077bda90383dde2
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-06-08 14:24:07 +0530

    update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,31 +3,8 @@ import Hero from '../../components/hero/Hero';
 import Biography from '../../components/biography/Biography';
 import Departments from '../../components/departments/Departments';
 import MessageForm from '../../components/messageForm/MessageForm';
-
-import { useContext, useEffect } from 'react';
-import axios from 'axios';
-import AuthenticatedContext from '../../contexts/AuthenticatedContext';
  
 const Home = () => {
-
-  const { setUser, isAuthenticated, setIsAuthenticated } = useContext(AuthenticatedContext);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await axios.get("https://hospital-management-skck.onrender.com/api/v1/user/patient/me", { withCredentials: true })
-        setIsAuthenticated(true)
-        setUser(res.data.user)
-        console.log("patient logged in successfully");
-      } catch (error) {
-        setIsAuthenticated(false)
-        setUser({})
-        console.log("SOME ERROE");
-      }
-    }
-    fetchUser();
-  }, [isAuthenticated])
-
 
   return (
     <>
