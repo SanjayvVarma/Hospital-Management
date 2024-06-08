b1a1e8c3f067d940a429b3179ad3deb880e15952
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-06-08 14:16:34 +0530

    update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,11 +4,35 @@ import Biography from '../../components/biography/Biography';
 import Departments from '../../components/departments/Departments';
 import MessageForm from '../../components/messageForm/MessageForm';
 
+import { useContext, useEffect } from 'react';
+import axios from 'axios';
+import AuthenticatedContext from '../../contexts/AuthenticatedContext';
+ 
 const Home = () => {
+
+  const { setUser, isAuthenticated, setIsAuthenticated } = useContext(AuthenticatedContext);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("https://hospital-management-skck.onrender.com/api/v1/user/patient/me", { withCredentials: true })
+        setIsAuthenticated(true)
+        setUser(res.data.user)
+        console.log("patient logged in successfully");
+      } catch (error) {
+        setIsAuthenticated(false)
+        setUser({})
+        console.log("SOME ERROE");
+      }
+    }
+    fetchUser();
+  }, [isAuthenticated])
+
+
   return (
     <>
       <Hero title={"Welcome to ZeeCare Medical Institute | Your Trusted Healthcare provider"} imageUrl={"/hero.png"} />
-      <Biography imageUrl={"/about.png"}/>
+      <Biography imageUrl={"/about.png"} />
       <Departments />
       <MessageForm />
     </>
