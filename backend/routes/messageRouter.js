2cbb2824628765e47ef9aa7d599cf85c1d941f33
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-06-08 23:20:48 +0530

    update

--- backend/routes/messageRouter.js
+++ backend/routes/messageRouter.js
@@ -4,7 +4,8 @@ import autheAutho from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/send", messageControllers.sendMessage)
-router.get("/getall", autheAutho.isAdminAuthenticated, messageControllers.getAllMessage)
+router.post("/send", messageControllers.sendMessage);
+router.get("/getall", autheAutho.isAdminAuthenticated, messageControllers.getAllMessage);
+router.delete("/delete/:id", autheAutho.isAdminAuthenticated, messageControllers.deleteMessage);
 
-export default router
+export default router;
--- dashboard/src/pages/message/Message.jsx
+++ dashboard/src/pages/message/Message.jsx
@@ -1,30 +1,41 @@
 import React, { useContext, useEffect, useState } from 'react';
-import axios from "axios"
-import DashboardContext from '../../contexts/DashboardContext'
 import { useNavigate } from 'react-router-dom';
+import { RiDeleteBinLine } from "react-icons/ri";
+import { toast } from "react-toastify";
+import axios from "axios";
+import DashboardContext from '../../contexts/DashboardContext';
 
 const Message = () => {
   const [messages, setMessages] = useState([]);
   const { isAuthenticated } = useContext(DashboardContext);
-
-  const navigateTo = useNavigate()
+  const navigateTo = useNavigate();
 
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const { data } = await axios.get("https://hospital-management-skck.onrender.com/api/v1/message/getall", { withCredentials: true, })
-        setMessages(data.message)
+        const { data } = await axios.get("https://hospital-management-skck.onrender.com/api/v1/message/getall", { withCredentials: true });
+        setMessages(data.message);
       } catch (error) {
-        console.log("ERROR OCCURED WILE FETCHING MESSAGE", error);
+        console.log("ERROR OCCURED WHILE FETCHING MESSAGE", error);
       }
+    };
+
+    if (!isAuthenticated) {
+      navigateTo("/login");
+    } else {
+      fetchMessages();
     }
+  }, [isAuthenticated, navigateTo]);
 
-    fetchMessages()
-  })
-
-  if (!isAuthenticated) {
-    navigateTo("/login")
-  }
+  const handleDeleteMessage = async (messageId) => {
+    try {
+      const { data } = await axios.delete(`https://hospital-management-skck.onrender.com/api/v1/message/delete/${messageId}`, { withCredentials: true });
+      setMessages((prevMessages) => prevMessages.filter((message) => message._id !== messageId));
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
 
   return (
     <section className="page messages">
@@ -35,31 +46,21 @@ const Message = () => {
             return (
               <div className="card" key={element._id}>
                 <div className="details">
-                  <p>
-                    First Name: <span>{element.firstName}</span>
-                  </p>
-                  <p>
-                    Last Name: <span>{element.lastName}</span>
-                  </p>
-                  <p>
-                    Email: <span>{element.email}</span>
-                  </p>
-                  <p>
-                    Phone: <span>{element.phone}</span>
-                  </p>
-                  <p>
-                    Message: <span>{element.message}</span>
-                  </p>
+                  <p > First Name: <span>{element.firstName}</span> </p>
+                  <p> Last Name: <span>{element.lastName}</span> </p>
+                  <p>  Email: <span>{element.email}</span> </p>
+                  <p>  Phone: <span>{element.phone}</span> </p>
+                  <p>Message: <span>{element.message}</span> </p>
                 </div>
+                <button onClick={() => handleDeleteMessage(element._id)} className="delete-button">
+                  <RiDeleteBinLine />
+                </button>
               </div>
             );
-          })
-        ) : (
-          <h1>No Messages!</h1>
-        )}
+          })) : (<h1>No Messages!</h1>)}
       </div>
     </section>
   )
 }
 
-export default Message
+export default Message;
