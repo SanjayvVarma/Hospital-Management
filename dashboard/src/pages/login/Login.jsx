36ba53be4acc0f3d70659e845b70ee9e1a693320
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-06-09 09:16:58 +0530

    loader

--- dashboard/src/pages/login/Login.jsx
+++ dashboard/src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
+import ClimbingBoxLoader from "react-spinners/ClipLoader";
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import DashboardContext from '../../contexts/DashboardContext.js';
@@ -10,12 +11,13 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-
+  const [loading, setLoading] = useState(false);
   const navigateTo = useNavigate();
 
 
   const handleLogin = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    setLoading(true);
     try {
       const res = await axios.post("https://hospital-management-skck.onrender.com/api/v1/user/login", { email, password, confirmPassword, role: "Admin" }, {
         withCredentials: true,
@@ -24,11 +26,12 @@ const Login = () => {
       toast.success(res.data.message)
       setUser(res.data.user)
       setIsAuthenticated(true)
-      // navigateTo("/")
+      navigateTo("/")
 
     } catch (error) {
       toast.error(error.response.data.message)
     }
+    setLoading(false)
   }
 
 
@@ -38,34 +41,37 @@ const Login = () => {
 
   return (
     <>
-      <section className="container form-component">
-        <img src="/logo.png" alt="logo" className="logo" />
-        <h1 className="form-title">WELCOME TO ZEECARE</h1>
-        <p>Only Admins Are Allowed To Access These Resources!</p>
-        <form onSubmit={handleLogin}>
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <div style={{ justifyContent: "center", alignItems: "center" }}>
-            <button type="submit">Login</button>
-          </div>
-        </form>
-      </section>
+      {
+        loading ? <ClimbingBoxLoader color="#36d7b7" size={100} /> :
+          <section className="container form-component">
+            <img src="/logo.png" alt="logo" className="logo" />
+            <h1 className="form-title">WELCOME TO ZEECARE</h1>
+            <p>Only Admins Are Allowed To Access These Resources!</p>
+            <form onSubmit={handleLogin}>
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <div style={{ justifyContent: "center", alignItems: "center" }}>
+                <button type="submit">Login</button>
+              </div>
+            </form>
+          </section>
+      }
     </>
   )
 }
