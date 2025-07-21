690f9d1a3351361e83a11a21dd9804059eb0d737
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-07-21 21:17:26 +0530

    Refactor Appointment component: implement conditional rendering for AppointmentForm and add login reminder styling.

--- frontend/src/pages/appointment/Appointment.jsx
+++ frontend/src/pages/appointment/Appointment.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import AppointmentForm from '../../components/appointmentForm/AppointmentForm'
-import Hero from '../../components/hero/Hero'
+import './appointment.scss';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../../components/hero/Hero';
+import { AuthContext, UserContext } from '../../contexts/AppContexts';
+import AppointmentForm from '../../components/appointmentForm/AppointmentForm';
 
 const Appointment = () => {
+  const { isAuth } = useContext(AuthContext);
+  const { user } = useContext(UserContext);
+
   return (
-    <>
+    <div>
       <Hero
-        title={"Schedule Your Appointment | ZeeCare Medical Institute"}
+        title={"Schedule Your Appointment | SkCare Medical Institute"}
         imageUrl={"/signin.png"}
       />
-      <AppointmentForm/>
-    </>
+      {isAuth && user ? (
+        <AppointmentForm />
+      ) : (
+        <div className="loginReminder">
+          <h2>Access Denied</h2>
+          <p>
+            To book an appointment, you must be logged in to your account.
+            Please <strong>log in</strong> to continue.
+          </p>
+          <p>If you don’t have an account, consider registering to access our full services.</p>
+          <Link to='/login' className="loginReminder__btn">
+            Go to Login Page
+          </Link>
+        </div>
+      )}
+    </div>
   )
 }
 
-export default Appointment
+export default Appointment;
