58e809edfb86f0a19a337e4f6aab3654e194bf2a
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-07-29 22:28:30 +0530

    add dashboard page

--- dashboard/src/pages/dashboard/Dashboard.jsx
+++ dashboard/src/pages/dashboard/Dashboard.jsx
@@ -1,158 +1,133 @@
 import axios from "axios";
+import "./dashboard.scss";
 import { toast } from 'react-toastify';
-import { RiDeleteBinLine } from "react-icons/ri";
-import { AiFillCloseCircle } from 'react-icons/ai';
-import { GoCheckCircleFill } from 'react-icons/go';
+import { Link, useNavigate } from "react-router-dom";
 import AuthContext from '../../contexts/AuthContext';
 import { useContext, useEffect, useState } from 'react';
-import { Navigate, useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
 
-  const { isAuth, user, setIsAuth } = useContext(AuthContext);
+  const [doctors, setDoctors] = useState([]);
+  const [patients, setPatients] = useState([]);
+  const [messages, setMessages] = useState([]);
   const [appointments, setAppointments] = useState([]);
-  const [doctorsRegistered, setDoctorsRegistered] = useState([]);
-  const navigateTo = useNavigate();
+
+  const { isAuth, user, setIsAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const fetchDoctors = async () => {
+    try {
+      const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/users/all-doctors`,
+        { withCredentials: true }
+      );
+
+      if (res.data.success) {
+        setDoctors(res.data.data);
+      }
+
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Doctor fetch failed");
+    }
+  };
+
+  const fetchPatients = async () => {
+    try {
+      const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/users/all-patient`,
+        { withCredentials: true }
+      );
+
+      if (res.data.success) {
+        setPatients(res.data.data);
+      }
+
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Patients fetch failed");
+    }
+  };
+
+  const fetchAppointments = async () => {
+    try {
+      const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/appointment/all-appointment`,
+        { withCredentials: true }
+      );
+
+      if (res.data.success) {
+        setAppointments(res.data.data);
+      }
+
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Appointment fetch failed");
+    }
+  };
+
+  const fetchMessages = async () => {
+    try {
+      const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/message/getall`,
+        { withCredentials: true }
+      );
+
+      if (res.data.success) {
+        setMessages(res.data.data);
+      }
+
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Messages fetch failed");
+    }
+  };
 
   useEffect(() => {
-    const registeredDoctors = async () => {
-      try {
-        const { data } = await axios.get("https://hospital-management-skck.onrender.com/api/v1/user/doctors", { withCredentials: true });
-        setDoctorsRegistered(data.doctors);
-      } catch (error) {
-        toast.error(error.response.data.message);
-      }
-    }
-    registeredDoctors();
-  }, []);
-
-  useEffect(() => {
-    const fetchAppointments = async () => {
-      try {
-        const { data } = await axios.get("https://hospital-management-skck.onrender.com/api/v1/appointment/getall", { withCredentials: true });
-        setAppointments(data.appointments);
-      } catch (error) {
-        setAppointments([]);
-        console.log("SAME ERROR OCCURED WHILE FETCHING APPOINTMENTS", error);
-      }
-    }
+    fetchDoctors();
+    fetchPatients();
+    fetchMessages();
     fetchAppointments();
   }, []);
 
-  const handleUpdateStatus = async (appointmentId, status) => {
-    try {
-      const { data } = await axios.put(`https://hospital-management-skck.onrender.com/api/v1/appointment/update/${appointmentId}`, { status }, { withCredentials: true });
-      setAppointments((prevAppointments) =>
-        prevAppointments.map((appointment) =>
-          appointment._id === appointmentId ? { ...appointment, status } : appointment
-        )
-      );
-      toast.success(data.message);
-    } catch (error) {
-      toast.error(error.response.data.message);
+  useEffect(() => {
+    if (!isAuth) {
+      navigate("/login");
     }
-  };
-
-  const gotoDoctorsPage = () => {
-    navigateTo("/doctors");
-    setShow(!show);
-  };
-
-
-  const handleDeleteAppoin = async (appointmentId) => {
-    try {
-      const { data } = await axios.delete(`https://hospital-management-skck.onrender.com/api/v1/appointment/delete/${appointmentId}`, { withCredentials: true });
-      setAppointments((prevAppointments) =>
-        prevAppointments.filter((appointment) => appointment._id !== appointmentId)
-      );
-      toast.success(data.message);
-    } catch (error) {
-      toast.error(error.response.data.message);
-    }
-  };
-
-  if (!isAuth) {
-    navigateTo("/login")
-  }
+  }, [isAuth, navigate]);
 
   return (
-    <>
-      <section className="dashboard page">
-        <div className="banner">
-          <div className="firstBox">
-            <img src="/doc.png" alt="docImg" />
-            <div className="content">
-              <div>
-                <p>Hello,</p>
-                <h5>{user && `${user.firstName} ${user.lastName}`}</h5>
-              </div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, nam molestias. Eaque molestiae ipsam commodi neque. Assumenda repellendus necessitatibus itaque. </p>
-            </div>
+    <div className="dashboardPage">
+      <div className="dashboardPage__welcome">
+        <img src={user.role === "Admin" ? "/admin.png" : "/doc.png"} alt="docImg" />
+        <div className="dashboardPage__welcome__name">
+          <div>
+            <p>Hello,</p>
+            <h5>{user && `${user.firstName} ${user.lastName}`}</h5>
           </div>
-          <div className="secondBox">
-            <p>Total Appointments</p>
-            <h3>{appointments.length}</h3>
-          </div>
-          <div className="thirdBox">
-            <p>Registered Doctors</p>
-            <h3 onClick={gotoDoctorsPage}>{doctorsRegistered.length}</h3>
-          </div>
+          <p>
+            1. Welcome to your personalized dashboard — your control center for managing doctors, patients, and appointments with ease and clarity.
+            <br />
+            2. Stay on top of everything happening in your system — track real-time activity, view progress, and take meaningful actions in just a few clicks.
+          </p>
         </div>
-        <div className="banner">
-          <h5>Appointments</h5>
-          <table>
-            <thead>
-              <tr>
-                <th>Patient</th>
-                <th>Date</th>
-                <th>Doctor</th>
-                <th>Department</th>
-                <th>Status</th>
-                <th>Visited</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {appointments && appointments.length > 0 ? (
-                appointments.map((appointment) => (
-                  <tr key={appointment._id}>
-                    <td>{`${appointment.firstName} ${appointment.lastName}`}</td>
-                    <td>{appointment.appointment_date.substring(0, 16)}</td>
-                    <td>{`${appointment.doctor.firstName} ${appointment.doctor.lastName}`}</td>
-                    <td>{appointment.department}</td>
-                    <td>
-                      <select
-                        className={appointment.status === "Pending" ? "value-pending" : appointment.status === "Accepted" ? "value-accepted" : "value-rejected"}
-                        value={appointment.status}
-                        onChange={(e) => handleUpdateStatus(appointment._id, e.target.value)}
-                      >
-                        <option value="Pending" className="value-pending"> Pending </option>
-                        <option value="Accepted" className="value-accepted"> Accepted </option>
-                        <option value="Rejected" className="value-rejected"> Rejected </option>
-                      </select>
-                    </td>
-                    <td>
-                      {appointment.hasVisited === true ?
-                        (<GoCheckCircleFill className="green" />) :
-                        (<AiFillCloseCircle className="red" />)
-                      }
-                    </td>
-                    <td>
-                      <button onClick={() => handleDeleteAppoin(appointment._id)} className="delete-button" >
-                        <RiDeleteBinLine />
-                      </button>
-                    </td>
-                  </tr>
-                ))) :
-                (<tr>
-                  <td colSpan="6" style={{ textAlign: "center" }}> No Appointments Found!</td>
-                </tr>
-                )}
-            </tbody>
-          </table>
-        </div>
-      </section>
-    </>
+      </div>
+
+      <div className="dashboardPage__details">
+        <Link to="/appointment" className="dashboardPage__details__appointment">
+          <p>Total Appointments</p>
+          <h3>{appointments.length}</h3>
+        </Link>
+
+        <Link to="/doctors" className="dashboardPage__details__doctors">
+          <p>Registered Doctors</p>
+          <h3>{doctors.length}</h3>
+        </Link>
+
+        <Link to="/patients" className="dashboardPage__details__patients">
+          <p>Registered Patients</p>
+          <h3>{patients.length}</h3>
+        </Link>
+
+        <Link to="/message" className="dashboardPage__details__messages">
+          <p>Messages</p>
+          <h3>{messages.length}</h3>
+        </Link>
+      </div>
+
+    </div>
   )
 };
 
