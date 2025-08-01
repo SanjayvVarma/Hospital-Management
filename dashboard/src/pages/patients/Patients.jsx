01863a053cfe7ac319e2d1eaf6c6b0f5a98d5c37
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-08-01 22:19:23 +0530

    add patients page

---
+++ dashboard/src/pages/patients/Patients.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Patients = () => {
+    return (
+        <div>
+            <h1>Coming soon...........</h1>
+        </div>
+    )
+}
+
+export default Patients;
